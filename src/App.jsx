1678326ce45349e7e99f31cad2a1b66d4463b1fd
Aminul Haqur Rana <aminulhaquerana1@gmail.com>
2024-10-12 23:43:27 +0600

    satting update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
-            <div    ><h1>
-              Lets Explore React
-              </h1>
-                                  </div>
-  )
+    <div>
+      <h1> Lets Explore React</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
